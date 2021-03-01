4a63e9553597b60dbe4baad470bba2a510e97e18
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2021-03-02 00:13:14 +0530

    Create cal.js

---
+++ cal.js
@@ -0,0 +1,6 @@
+prompt("what is your name");
+prompt("What is there name");
+
+var lovescore = Math.random() + 100;
+lovescore = Math.floor(lovescore) + 1;
+console.log("your love score is " + lovescore + "%");
